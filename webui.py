19a82ab256b55c4d82c8b96eb28b88fa114625a2
chendong0444 <54466988@qq.com>
2023-08-12 22:04:09 +0800

    translate

--- webui.py
+++ webui.py
@@ -377,7 +377,7 @@ def api_only():
 
     request = StableDiffusionTxt2ImgProcessingAPI()
     request.save_images = True
-    prompt = '白色透明情趣睡衣, 大长腿, 白皙的皮肤, 苗条,裸体,(漂亮:1.5) 女模特, 特写,在酒店房间里,坐在床上,背LV包,包带在双乳之间,(丰满的双乳:1.5)'
+    prompt = '透明, 白皙的皮肤, 苗条,裸体,(漂亮:1.5)空姐, 特写,在酒店房间里,坐在床上,背LV包,包带在双乳之间,(丰满的双乳:1.5)'
     prompt = translate(prompt)
     print(prompt)
     request.prompt = f'''
